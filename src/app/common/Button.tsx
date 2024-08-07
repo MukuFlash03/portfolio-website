f041e6703666f86e1dd077bfe30ee6d0a9f2dfc9
Mukul Chandrakant Mahadik <mmahadik@asu.edu>
2024-08-07 12:27:48 -0700

    Changed view resume button color.

--- src/app/common/Button.tsx
+++ src/app/common/Button.tsx
@@ -11,7 +11,7 @@ const Button: React.FC<{
     return (
       <div className="w-full flex flex-col items-center justify-center space-y-2.5 px-1 md:px-5 py-2.5">
         <Link href="/resumeMukul.pdf" target="_blank" rel="noopener noreferrer">
-          <button className="w-[110px] md:w-[135px] h-[30px] md:h-[50px] bg-[#4C0693] rounded-full flex items-center justify-center overflow-hidden">
+          <button className="w-[110px] md:w-[135px] h-[30px] md:h-[50px] bg-[#2c0d71] rounded-full flex items-center justify-center overflow-hidden">
             <span className="text-xs md:text-sm text-white whitespace-nowrap px-2">{label}</span>
           </button>
         </Link>
