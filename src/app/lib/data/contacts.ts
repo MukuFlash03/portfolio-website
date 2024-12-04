c814a263737dfe70a7dbb624130bf3213ccbe494
Mukul Chandrakant Mahadik <mmahadik@asu.edu>
2024-12-03 18:51:26 -0800

    Updated Resume + changed email address.

--- src/app/lib/data/contacts.ts
+++ src/app/lib/data/contacts.ts
@@ -4,7 +4,7 @@ import { Contact } from "@/app/lib/types/contact";
 const contactsData: Contact[] = [
   { name: 'LinkedIn', logo: contactLogos.linkedin, url: 'https://www.linkedin.com/in/mukulm03', },
   { name: 'GitHub', logo: contactLogos.github, url: 'https://github.com/MukuFlash03', },
-  { name: 'Gmail', logo: contactLogos.gmail, url: 'mailto:mukulm2010@gmail.com', ratio: true },
+  { name: 'Gmail', logo: contactLogos.gmail, url: 'mailto:mukul.jobs.dev@gmail.com', ratio: true },
   { name: 'Wordpress', logo: contactLogos.wordpress, url: 'https://mukulcodes.wordpress.com', },
 ];
 
